81ef7e495990db3a3bb4de092b632950f51b5a81
taimg1 <oleg.s2822@gmail.com>
2024-06-16 15:23:33 +0300

    feat: 11 lab finish

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Blog\PostController;
 use App\Http\Controllers\Blog\Admin\PostController as AdminPostController;
+use App\Http\Controllers\DiggingDeeperController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,6 +21,14 @@ Route::middleware([
     })->name('dashboard');
 });
 Route::resource('rest', RestTestController::class)->names('restTest');
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
+});
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
