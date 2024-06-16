460ba1f59c62276cb396d40cd06775680df66410
taimg1 <oleg.s2822@gmail.com>
2024-06-16 15:40:11 +0300

    feat: 12 lab finish

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -9,9 +9,14 @@ use App\Http\Requests\BlogPostUpdateRequest;
 use Illuminate\Support\Str;
 use App\Models\BlogPost;
 use App\Http\Requests\BlogPostCreateRequest;
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
 
 class PostController extends BaseController
 {
+    use DispatchesJobs;
     /**
      * @var BlogPostRepository
      */
@@ -60,6 +65,8 @@ class PostController extends BaseController
         $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
 
         if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            $this->dispatch($job);
             return redirect()
                 ->route('blog.admin.posts.edit', [$item->id])
                 ->with(['success' => 'Успішно збережено']);
@@ -134,6 +141,7 @@ class PostController extends BaseController
         //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
 
         if ($result) {
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
             return redirect()
                 ->route('blog.admin.posts.index')
                 ->with(['success' => "Запис id[$id] видалено"]);
---
+++ app/Jobs/BlogPostAfterCreateJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\BlogPost;
+
+class BlogPostAfterCreateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    /**
+     * @var BlogPost
+     */
+    private $blogPost;
+    public function __construct(BlogPost $blogPost)
+    {
+        $this->blogPost = $blogPost;
+    }
+
+    public function handle()
+    {
+        logs()->info("Створено новий запис в блозі [{$this->blogPost->id}]");
+    }
+}
